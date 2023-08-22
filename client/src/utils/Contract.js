3593e1eb5c6c8a88614d9397ef33824e8f7a8b2c
Muhammad Danish Zaki <125446507+danish199327@users.noreply.github.com>
2023-08-22 20:38:42 +0500

    Add files via upload

---
+++ client/src/utils/Contract.js
@@ -0,0 +1,4 @@
+import abi from "../../../Smart-Contract/artifacts/contracts/Transaction.sol/Transaction.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0xaE1979849a40ED78C63F0D03E037Cd26522Ab7cf";
